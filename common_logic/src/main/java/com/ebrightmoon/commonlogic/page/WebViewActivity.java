74fafe6ab4c75c5ced6b865db5ffcc4324466be8
ebrightmoon <2315813288@qq.com>
2020-01-17 15:14:01 +0800

    动态修改包明，app名称和应用图标

--- common_logic/src/main/java/com/ebrightmoon/commonlogic/page/WebViewActivity.java
+++ common_logic/src/main/java/com/ebrightmoon/commonlogic/page/WebViewActivity.java
@@ -1,6 +1,5 @@
 package com.ebrightmoon.commonlogic.page;
 
-import android.annotation.SuppressLint;
 import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
@@ -8,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.SslErrorHandler;
-import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
